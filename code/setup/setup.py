6729cf7d528e7b24174f0f0945c4b811ef5a2934
tiago.blima <tiago.blima02@gmail.com>
2020-10-21 07:24:40 -0300

    Cleaning the setup.py

--- code/setup/setup.py
+++ code/setup/setup.py
@@ -1,147 +1,17 @@
 # Try experimenting with the size of that dataset
 import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
-import tensorflow_datasets as tfds
-import os
-from layers.layers import Encoder, EncoderLayer, Transformer
-from util.util import create_masks, positional_encoding, point_wise_feed_forward_network, CustomSchedule, loss_function
 
-BASE_DIR = 'Indigenous-Languages-Resources/'
-train_examples = open(os.path.join(BASE_DIR, 'training/Guarani-Portugues.txt'), encoding='utf8').readlines()
-val_examples = open(os.path.join(BASE_DIR, 'validation/Guarani-Portugues.txt'), encoding='utf8').readlines()
+from layers.layers import Transformer
+from util.util import create_masks, CustomSchedule, loss_function
 
-print(train_examples[:1])
-
-train_examples_pt = [ex.strip().split('\t')[0] for ex in train_examples]
-train_examples_gn = [ex.strip().split('\t')[1] for ex in train_examples]
-
-val_examples_pt = [ex.strip().split('\t')[0] for ex in val_examples]
-val_examples_gn = [ex.strip().split('\t')[1] for ex in val_examples]
-print(train_examples_pt[:1])
-print(train_examples_gn[:1])
-
-train_examples = tf.data.Dataset.from_tensor_slices((np.array(train_examples_pt),
-                                                     np.array(train_examples_gn)))
-
-val_examples = tf.data.Dataset.from_tensor_slices((val_examples_pt, val_examples_gn))
-print(train_examples)
-
-tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-    (en.numpy() for pt, en in train_examples), target_vocab_size=2 ** 13)
-
-tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-    (pt.numpy() for pt, en in train_examples), target_vocab_size=2 ** 13)
-
-sample_string = 'Ejojuka eme.'
-
-tokenized_string = tokenizer_en.encode(sample_string)
-print('Tokenized string is {}'.format(tokenized_string))
-
-original_string = tokenizer_en.decode(tokenized_string)
-print('The original string: {}'.format(original_string))
-
-assert original_string == sample_string
-for ts in tokenized_string:
-    print('{} ----> {}'.format(ts, tokenizer_en.decode([ts])))
-
-BUFFER_SIZE = 20000
-BATCH_SIZE = 64
+from .load_corpus import tokenizer_inp, tokenizer_targ,MAX_LENGTH
+from .params import *
 
 
-def encode(lang1, lang2):
-    lang1 = [tokenizer_pt.vocab_size] + tokenizer_pt.encode(
-        lang1.numpy()) + [tokenizer_pt.vocab_size + 1]
+input_vocab_size = tokenizer_targ.vocab_size + 2
+target_vocab_size = tokenizer_inp.vocab_size + 2
 
-    lang2 = [tokenizer_en.vocab_size] + tokenizer_en.encode(
-        lang2.numpy()) + [tokenizer_en.vocab_size + 1]
-
-    return lang1, lang2
-
-
-def tf_encode(pt, en):
-    result_pt, result_en = tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
-    result_pt.set_shape([None])
-    result_en.set_shape([None])
-
-    return result_pt, result_en
-
-
-MAX_LENGTH = 40
-
-
-def filter_max_length(x, y, max_length=MAX_LENGTH):
-    return tf.logical_and(tf.size(x) <= max_length,
-                          tf.size(y) <= max_length)
-
-
-train_dataset = train_examples.map(tf_encode)
-train_dataset = train_dataset.filter(filter_max_length)
-# cache the dataset to memory to get a speedup while reading from it.
-train_dataset = train_dataset.cache()
-train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE)
-train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
-val_dataset = val_examples.map(tf_encode)
-val_dataset = val_dataset.filter(filter_max_length).padded_batch(BATCH_SIZE)
-
-
-def filter_max_length(x, y, max_length=MAX_LENGTH):
-    return tf.logical_and(tf.size(x) <= max_length,
-                          tf.size(y) <= max_length)
-
-
-pt_batch, en_batch = next(iter(val_dataset))
-
-pos_encoding = positional_encoding(50, 512)
-print(pos_encoding.shape)
-
-plt.pcolormesh(pos_encoding[0], cmap='RdBu')
-plt.xlabel('Depth')
-plt.xlim((0, 512))
-plt.ylabel('Position')
-plt.colorbar()
-plt.show()
-
-sample_ffn = point_wise_feed_forward_network(512, 2048)
-print(sample_ffn(tf.random.uniform((64, 50, 512))).shape)
-
-sample_encoder_layer = EncoderLayer(512, 8, 2048)
-
-sample_encoder_layer_output = sample_encoder_layer(
-    tf.random.uniform((64, 43, 512)), False, None)
-sample_encoder = Encoder(num_layers=2, d_model=512, num_heads=8,
-                         dff=2048, input_vocab_size=8500,
-                         maximum_position_encoding=10000)
-temp_input = tf.random.uniform((64, 62), dtype=tf.int64, minval=0, maxval=200)
-
-sample_encoder_output = sample_encoder(temp_input, training=False, mask=None)
-
-print(sample_encoder_output.shape)  # (batch_size, input_seq_len, d_model)
-
-sample_transformer = Transformer(
-    num_layers=2, d_model=512, num_heads=8, dff=2048,
-    input_vocab_size=8500, target_vocab_size=8000,
-    pe_input=10000, pe_target=6000)
-
-temp_input = tf.random.uniform((64, 38), dtype=tf.int64, minval=0, maxval=200)
-temp_target = tf.random.uniform((64, 36), dtype=tf.int64, minval=0, maxval=200)
-
-fn_out, _ = sample_transformer(temp_input, temp_target, training=False,
-                               enc_padding_mask=None,
-                               look_ahead_mask=None,
-                               dec_padding_mask=None)
-
-print(fn_out.shape)  # (batch_size, tar_seq_len, target_vocab_size)
-
-num_layers = 6
-d_model = 512
-dff = 2048
-num_heads = 8
-
-input_vocab_size = tokenizer_pt.vocab_size + 2
-target_vocab_size = tokenizer_en.vocab_size + 2
-dropout_rate = 0.1
 
 learning_rate = CustomSchedule(d_model)
 
@@ -167,6 +37,7 @@ train_step_signature = [
     tf.TensorSpec(shape=(None, None), dtype=tf.int64),
     tf.TensorSpec(shape=(None, None), dtype=tf.int64),
 ]
+
 checkpoint_path = "/content/drive/My Drive/checkpoints/train"
 
 ckpt = tf.train.Checkpoint(transformer=transformer,
@@ -204,16 +75,16 @@ def train_step(inp, tar):
 
 def evaluate(inp_sentence):
     attention_weights = None
-    start_token = [tokenizer_pt.vocab_size]
-    end_token = [tokenizer_pt.vocab_size + 1]
+    start_token = [tokenizer_targ.vocab_size]
+    end_token = [tokenizer_targ.vocab_size + 1]
 
     # inp sentence is portuguese, hence adding the start and end token
-    inp_sentence = start_token + tokenizer_pt.encode(inp_sentence) + end_token
+    inp_sentence = start_token + tokenizer_targ.encode(inp_sentence) + end_token
     encoder_input = tf.expand_dims(inp_sentence, 0)
 
     # as the target is english, the first word to the transformer should be the
     # english start token.
-    decoder_input = [tokenizer_en.vocab_size]
+    decoder_input = [tokenizer_inp.vocab_size]
     output = tf.expand_dims(decoder_input, 0)
 
     for i in range(MAX_LENGTH):
@@ -234,7 +105,7 @@ def evaluate(inp_sentence):
         predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
 
         # return the result if the predicted_id is equal to the end token
-        if predicted_id == tokenizer_en.vocab_size + 1:
+        if predicted_id == tokenizer_inp.vocab_size + 1:
             return tf.squeeze(output, axis=0), attention_weights
 
         # concatentate the predicted_id to the output which is given to the decoder
@@ -247,7 +118,7 @@ def evaluate(inp_sentence):
 def plot_attention_weights(attention, sentence, result, layer):
     fig = plt.figure(figsize=(16, 8))
 
-    sentence = tokenizer_pt.encode(sentence)
+    sentence = tokenizer_targ.encode(sentence)
 
     attention = tf.squeeze(attention[layer], axis=0)
 
@@ -265,11 +136,11 @@ def plot_attention_weights(attention, sentence, result, layer):
         ax.set_ylim(len(result) - 1.5, -0.5)
 
         ax.set_xticklabels(
-            ['<start>'] + [tokenizer_pt.decode([i]) for i in sentence] + ['<end>'],
+            ['<start>'] + [tokenizer_targ.decode([i]) for i in sentence] + ['<end>'],
             fontdict=fontdict, rotation=90)
 
-        ax.set_yticklabels([tokenizer_en.decode([i]) for i in result
-                            if i < tokenizer_en.vocab_size],
+        ax.set_yticklabels([tokenizer_inp.decode([i]) for i in result
+                            if i < tokenizer_inp.vocab_size],
                            fontdict=fontdict)
 
         ax.set_xlabel('Head {}'.format(head + 1))
@@ -281,8 +152,8 @@ def plot_attention_weights(attention, sentence, result, layer):
 def translate(sentence, plot=''):
     result, attention_weights = evaluate(sentence)
 
-    predicted_sentence = tokenizer_en.decode([i for i in result
-                                              if i < tokenizer_en.vocab_size])
+    predicted_sentence = tokenizer_inp.decode([i for i in result
+                                               if i < tokenizer_inp.vocab_size])
 
     # print('Input: {}'.format(sentence))
     # print('Predicted translation: {}'.format(predicted_sentence))
