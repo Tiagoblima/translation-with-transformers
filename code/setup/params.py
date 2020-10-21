9177b08bc9852c6b954a11503cd52ac49d71c857
tiago.blima <tiago.blima02@gmail.com>
2020-10-21 07:24:25 -0300

    gathering the params in a file

---
+++ code/setup/params.py
@@ -0,0 +1,12 @@
+BASE_DIR = 'Indigenous-Languages-Resources/'
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+MAX_LENGTH = 40
+
+num_layers = 6
+d_model = 512
+dff = 2048
+num_heads = 8
+dropout_rate = 0.1
