980864ee3c6a07a1b5280aaf70d7071328aa5d58
tiago.blima <tiago.blima02@gmail.com>
2020-10-31 16:05:16 -0300

    Fixing testing routine

--- code/model/testing.py
+++ code/model/testing.py
@@ -3,39 +3,40 @@ from nltk.translate.bleu_score import SmoothingFunction
 import sys
 import os
 import json
-
+import numpy as np
 from model.params import BASE_DIR, BATCH_SIZE
 from model.training import translate
+
+smoth = SmoothingFunction()
 
 
 def test_sentence(args):
     sentence, references = args
     predicted = translate(sentence)
 
-    return predicted.lower().split(), references
+    return sentence_bleu(references, predicted, smoothing_function=smoth.method4)
 
 
 def split_batches(corpus, batch_size):
     previous = 0
-    for i in range(len(corpus), batch_size):
-        yield corpus[previous:i]
+    batches = []
+    for i in range(batch_size, len(corpus), batch_size):
+        batches.append(corpus[previous:i])
         previous = i
+    return batches
 
 
 def test():
     test_file = os.path.join(BASE_DIR, 'testing/test_corpus_gn-pt.json')
 
-    test_corpus = list(json.load(open(test_file, encoding='utf-8')).values())[:64]
+    test_corpus = list(json.load(open(test_file, encoding='utf-8')).values())
     batches = split_batches(test_corpus, BATCH_SIZE)
     history = {}
     for i, batch in enumerate(batches):
         results = list(map(test_sentence, batch))
-        predictions = [res[0] for res in results]
-        true_translations = [res[1] for res in results]
-        smoth = SmoothingFunction()
-        score = corpus_bleu(true_translations, predictions, smoothing_function=smoth.method4)
+
         history[i] = {'inputs': batch,
-                      'predictions': predictions}
-        print("\nCorpus score: ", score)
+                      'scores': results}
+        print("\nCorpus score: ", np.mean(results), np.std(results))
         if i % 5 == 0:
             json.dump(history, open('scores_history.json', 'w'))
